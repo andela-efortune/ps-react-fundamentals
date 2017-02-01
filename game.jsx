5b8ac8a367177c222b8f0d66b0d6ee7340565f93
Fortune Ekeruo <legalmody@gmail.com>
2017-02-01 12:23:33 +0100

    Add a game component

---
+++ game.jsx
@@ -0,0 +1,15 @@
+const Game = React.createClass({
+  render() {
+    return (
+      <div id="game">
+        <h2>Play Nine</h2>
+
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('container');
+);
