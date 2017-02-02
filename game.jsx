0eb7fd069951c37370666a0cca1a833b42f49ccf
Fortune Ekeruo <legalmody@gmail.com>
2017-02-02 18:34:47 +0100

    Changing or unselecting an answer

--- game.jsx
+++ game.jsx
@@ -29,10 +29,10 @@ const ButtonFrame = React.createClass({
 
 const AnswerFrame = React.createClass({
   render() {
-    let props = this.props;
+    // let props = this.props;
     let selectedNumbers = this.props.selectedNumbers.map((num) => {
       return (
-        <span>{num}</span>
+        <span onClick={this.props.unselectNumber.bind(null, num)}>{num}</span>
       );
     });
 
@@ -49,13 +49,13 @@ const AnswerFrame = React.createClass({
 const NumbersFrame = React.createClass({
   render() {
     let numbers = [], className,
-        clickNumber = this.props.clickNumber,
+        selectNumber = this.props.selectNumber,
         selectedNumbers = this.props.selectedNumbers;
 
     for (let i = 1; i <= 9; i++) {
       className = "number selected-" + (selectedNumbers.indexOf(i) >= 0);
       numbers.push(
-        <div className={className} onClick={clickNumber.bind(null, i)}>{i}</div>
+        <div className={className} onClick={selectNumber.bind(null, i)}>{i}</div>
       );
     }
     return (
@@ -76,12 +76,25 @@ const Game = React.createClass({
            };
   },
 
-  clickNumber(clickedNumber) {
+  selectNumber(clickedNumber) {
+    // Only select a number once.
     if (this.state.selectedNumbers.indexOf(clickedNumber) < 0) {
       this.setState(
         { selectedNumbers: this.state.selectedNumbers.concat(clickedNumber) }
       );
     }
+  },
+
+  unselectNumber(clickedNumber) {
+    // Get selectedNumbers state and indexOf of number to unselect
+    let selectedNumbers = this.state.selectedNumbers,
+        indexOfNum = selectedNumbers.indexOf(clickedNumber);
+
+    // Remove clickedNumber from selectedNumbers
+    selectedNumbers.splice(indexOfNum, 1);
+
+    // Update state of selectedNumbers
+    this.setState({ selectedNumbers: selectedNumbers });
   },
 
   render() {
@@ -92,11 +105,12 @@ const Game = React.createClass({
         <div className="clearfix">
           <StarFrame numOfStars={this.state.numOfStars}/>
           <ButtonFrame />
-          <AnswerFrame selectedNumbers={this.state.selectedNumbers} />
+          <AnswerFrame selectedNumbers={this.state.selectedNumbers}
+                       unselectNumber={this.unselectNumber}/>
         </div>
 
         <NumbersFrame selectedNumbers={this.state.selectedNumbers}
-                      clickNumber={this.clickNumber} />
+                      selectNumber={this.selectNumber} />
       </div>
     );
   }
